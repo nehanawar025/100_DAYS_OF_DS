3e800e7ec5ecc83ed2e01bdc21e0a27bf3a99b85
Noshin Nawar Neha <99127699+nehanawar025@users.noreply.github.com>
2024-03-14 17:41:55 +0600

    Update 2882.py

--- Day 1/Pandas/2882.py
+++ Day 1/Pandas/2882.py
@@ -1,13 +1,3 @@
-# DataFrame customers
-# +-------------+--------+
-# | Column Name | Type   |
-# +-------------+--------+
-# | customer_id | int    |
-# | name        | object |
-# | email       | object |
-# +-------------+--------+
-# There are some duplicate rows in the DataFrame based on the email column.
-
 # Write a solution to remove these duplicate rows and keep only the first occurrence.
 
 # The result format is in the following example.
