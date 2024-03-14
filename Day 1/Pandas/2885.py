378c3a632decff3b7449c0b9bd6eec6907bef431
Noshin Nawar Neha <99127699+nehanawar025@users.noreply.github.com>
2024-03-14 17:09:17 +0600

    Create 2885.py

---
+++ Day 1/Pandas/2885.py
@@ -0,0 +1,15 @@
+# Write a solution to rename the columns as follows:
+
+# id to student_id
+# first to first_name
+# last to last_name
+# age to age_in_years
+
+# ----------------------------------------------------------------------------
+
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    students.rename(columns = {'id':'student_id','first': 'first_name',
+    'last' : 'last_name', 'age':'age_in_years'}, inplace =True)
+    return students
